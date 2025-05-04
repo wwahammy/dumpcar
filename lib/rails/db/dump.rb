64a9f779060a8bc2f9195f5b50da04bde14e23cc
Eric Schultz <eric@wwahammy.com>
2025-05-04 14:28:06 -0500

    Initial commit

---
+++ lib/rails/db/dump.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "dump/version"
+
+module Rails
+  module Db
+    module Dump
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
