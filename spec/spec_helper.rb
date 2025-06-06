161a5673c2bddc6e4d31898de35f7cd248b7a36c
Eric Schultz <eric@wwahammy.com>
2025-06-06 18:38:16 +0000

    Add aruba config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,3 +35,8 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+::Dir.glob(::File.expand_path("../support/*.rb", __FILE__)).each { |f| require_relative f }
+::Dir.glob(::File.expand_path("../support/**/*.rb", __FILE__)).each { |f| require_relative f }
