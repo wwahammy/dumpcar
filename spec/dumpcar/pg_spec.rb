69971ef24ccd74360218e322352e201d49a4cba5
Eric Schultz <eric@wwahammy.com>
2025-06-14 02:07:11 +0000

    Add basic `Dumpcar::Pg` specs

--- spec/dumpcar/pg_spec.rb
+++ spec/dumpcar/pg_spec.rb
@@ -1,3 +1,21 @@
 RSpec.describe Dumpcar::Pg do
-  it ""
+  describe ":dump" do
+    it "does not crash" do
+      pg = described_class.new(Dumpcar::Util.get_connection_db_config)
+
+      expect { pg.dump(Rails.root.join("tmp/#{Random.uuid}.dump").to_s) }.to_not raise_error
+    end
+
+    it "properly dumps an item"
+  end
+
+  describe ":restore" do
+    it "does not crash" do
+      pg = described_class.new(Dumpcar::Util.get_connection_db_config)
+
+      expect { pg.restore(Dumpcar::Location.new(Rails.root.join("db/dumps")).last) }.to_not raise_error
+    end
+
+    it "properly restores an item"
+  end
 end
