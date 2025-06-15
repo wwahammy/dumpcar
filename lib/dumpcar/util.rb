ba5d24a12e4f1203316f043c08bc3a60e20439bf
Eric Schultz <eric@wwahammy.com>
2025-06-15 21:55:40 +0000

    Use logger for the output of terrapin

--- lib/dumpcar/util.rb
+++ lib/dumpcar/util.rb
@@ -2,4 +2,8 @@ module Dumpcar::Util
   def self.get_connection_db_config
     (Rails.version < "6.1") ? ActiveRecord::Base.connection_config : ActiveRecord::Base.connection_db_config.configuration_hash
   end
+
+  def self.logger
+    Rails.logger
+  end
 end
