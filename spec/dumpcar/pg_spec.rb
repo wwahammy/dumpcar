333ffe76547be07ab5f46e4532e928c61a1e673d
Eric Schultz <eric@wwahammy.com>
2025-06-15 21:45:29 +0000

    Improve tests for dump and restore

--- spec/dumpcar/pg_spec.rb
+++ spec/dumpcar/pg_spec.rb
@@ -6,7 +6,23 @@ RSpec.describe Dumpcar::Pg do
       pg.dump(Rails.root.join("tmp/#{Random.uuid}.dump").to_s)
     end
 
-    it "properly dumps an item"
+    it "properly dumps and restores an item" do
+      pg = described_class.new(Dumpcar::Util.get_connection_db_config)
+
+      expect(SimpleObject.count).to eq 0
+      SimpleObject.create(name: "so-1")
+
+      dump_file = Rails.root.join("tmp/#{Random.uuid}.dump").to_s
+
+      pg.dump(dump_file)
+
+      SimpleObject.delete_all
+
+      expect(SimpleObject.count).to eq 0
+      pg.restore(dump_file)
+
+      expect(SimpleObject.count).to eq 1
+    end
   end
 
   describe ":restore" do
@@ -16,6 +32,13 @@ RSpec.describe Dumpcar::Pg do
       expect { pg.restore(Dumpcar::Location.new(Rails.root.join("db/dumps")).last) }.to_not raise_error
     end
 
-    it "properly restores an item"
+    it "properly restores an item" do
+      pg = described_class.new(Dumpcar::Util.get_connection_db_config)
+
+      expect(SimpleObject.count).to eq 0
+      pg.restore(Dumpcar::Location.new(Rails.root.join("db/dumps")).last)
+
+      expect(SimpleObject.count).to eq 3
+    end
   end
 end
